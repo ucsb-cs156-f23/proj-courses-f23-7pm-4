3c64623bf70ac008290d448480dd792382aa4639
apzhou0 <apzhou@ucsb.edu>
2023-11-20 16:20:38 -0800

    azhou - Added CourseDetailsPage placeholder that displays the quarter information and enroll code for a course

---
+++ frontend/src/main/pages/CourseDetails/CourseDetailsPage.js
@@ -0,0 +1,17 @@
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import { useParams } from "react-router-dom";
+//import { useBackend, _useBackendMutation } from "main/utils/useBackend";
+
+export default function CourseDetailsPage() {
+  let { qyy, enrollCd } = useParams();
+
+  return (
+    <BasicLayout>
+      <div className="pt-2">
+        <h1>Course Details Placeholder Page</h1>
+        <p>Quarter in qyy format: {qyy} </p>
+        <p>EnrollCd: {enrollCd} </p>
+      </div>
+    </BasicLayout>
+  )
+}
