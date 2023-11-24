fadbaafd4ff7e66403c80bd19d1bd47784f419a9
apzhou0 <apzhou@ucsb.edu>
2023-11-24 11:54:32 -0800

    azhou - Removed params from API call

--- frontend/src/main/pages/CourseDetails/CourseDetailsPage.js
+++ frontend/src/main/pages/CourseDetails/CourseDetailsPage.js
@@ -4,30 +4,23 @@ import CourseDetailsTable from "main/components/Courses/CourseDetailsTable";
 import { useParams } from "react-router-dom";
 
 export default function CourseDetailsPage() {
-  const { qyy, enrollCd } = useParams();
+  const { yyyyq, enrollCd } = useParams();
 
   const {
-    data: personalSection,
-    error: _error,
-    status: _status,
+    data: personalSection
   } = useBackend(
     // Stryker disable all : hard to test for query caching
-    [`/api/sections/sectionsearch?qtr=${qyy}&enrollCode=${enrollCd}`],
+    [`/api/sections/sectionsearch?qtr=${yyyyq}&enrollCode=${enrollCd}`],
     {
       // Stryker disable next-line all : GET is the default, so changing this to "" doesn't introduce a bug
       method: "GET",
-      url: `/api/sections/sectionsearch`,
-      params: {
-        qtr: qyy,
-        enrollCode: enrollCd,
-      },
+      url: `/api/sections/sectionsearch?qtr=${yyyyq}&enrollCode=${enrollCd}`,
     },
-    [],
   );
 
   return (
     <BasicLayout>
-      <div>
+      <div className="pt-2">
         <h1>Course Details Placeholder Page</h1>
         {personalSection && <CourseDetailsTable course={[personalSection]} />}
       </div>
