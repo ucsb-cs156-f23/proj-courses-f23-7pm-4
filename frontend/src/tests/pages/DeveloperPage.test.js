d77084c4e26443f55e8713138124f730828009e4
ericmarzouk <ericmarzouk@ucsb.edu>
2023-11-25 01:18:17 -0800

    ran prettier

--- frontend/src/tests/pages/DeveloperPage.test.js
+++ frontend/src/tests/pages/DeveloperPage.test.js
@@ -8,28 +8,27 @@ import AxiosMockAdapter from "axios-mock-adapter";
 import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
 import DeveloperPage from "main/pages/DeveloperPage";
 
-
 describe("DeveloperPage tests", () => {
-    const queryClient = new QueryClient();
-  
-    test("renders correctly", async () => {
-      const axiosMock = new AxiosMockAdapter(axios);
-      axiosMock
-        .onGet("/api/currentUser")
-        .reply(200, apiCurrentUserFixtures.userOnly);
-      axiosMock
-        .onGet("/api/systemInfo")
-        .reply(200, systemInfoFixtures.showingNeither);
-  
-      render(
-        <QueryClientProvider client={queryClient}>
-          <MemoryRouter>
-            <DeveloperPage />
-          </MemoryRouter>
-        </QueryClientProvider>,
-      );
-      expect(screen.getByText("Welcome to the Developer Page!")).toBeInTheDocument();
+  const queryClient = new QueryClient();
 
-    });
+  test("renders correctly", async () => {
+    const axiosMock = new AxiosMockAdapter(axios);
+    axiosMock
+      .onGet("/api/currentUser")
+      .reply(200, apiCurrentUserFixtures.userOnly);
+    axiosMock
+      .onGet("/api/systemInfo")
+      .reply(200, systemInfoFixtures.showingNeither);
+
+    render(
+      <QueryClientProvider client={queryClient}>
+        <MemoryRouter>
+          <DeveloperPage />
+        </MemoryRouter>
+      </QueryClientProvider>,
+    );
+    expect(
+      screen.getByText("Welcome to the Developer Page!"),
+    ).toBeInTheDocument();
   });
-  
+});
