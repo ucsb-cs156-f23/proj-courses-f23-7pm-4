a05d0f221e40f8e5a3ce695ff81220c536dbe680
benjaminconte <benjaminconte@ucsb.edu>
2023-11-27 19:47:56 -0800

    added api

---
+++ src/main/java/edu/ucsb/cs156/courses/repositories/FinalsInfoRepository.java
@@ -0,0 +1,11 @@
+package edu.ucsb.cs156.courses.repositories;
+
+import edu.ucsb.cs156.courses.entities.FinalsInfo;
+
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FinalsInfoRepository extends CrudRepository<FinalsInfo, Long> {}
