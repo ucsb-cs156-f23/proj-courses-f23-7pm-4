198b0585700e500683112cb5cb404bddcd02ef7b
benjaminconte <benjaminconte@ucsb.edu>
2023-11-30 14:50:45 -0800

    added return:

--- src/main/java/edu/ucsb/cs156/courses/controllers/FinalsInfoController.java
+++ src/main/java/edu/ucsb/cs156/courses/controllers/FinalsInfoController.java
@@ -1,6 +1,7 @@
 package edu.ucsb.cs156.courses.controllers;
 
 import edu.ucsb.cs156.courses.repositories.FinalsInfoRepository;
+import edu.ucsb.cs156.courses.services.UCSBCurriculumService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -19,11 +20,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class FinalsInfoController extends ApiController {
 
   @Autowired private FinalsInfoRepository finalsInfoRepository;
+  @Autowired private UCSBCurriculumService UCS;
 
   @GetMapping("")
   @PreAuthorize("hasRole('ROLE_USER')")
   @Operation(summary = "Get finals information for a specific course")
-  public void getFinalsInfo(
+  public String getFinalsInfo(
       @Parameter(
               name = "quarter",
               description = "Quarter identifier, e.g., 'f23' for Fall 2023",
@@ -37,5 +39,7 @@ public class FinalsInfoController extends ApiController {
               example = "12345",
               required = true)
           @RequestParam
-          String enrollCd) {}
+          String enrollCd) {
+    return UCS.getFinalsJSON("f23", "12345");
+  }
 }
